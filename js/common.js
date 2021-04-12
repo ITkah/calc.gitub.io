9e3af0e5b7393825662a3bb74984ecb460f40613
ITkah <ilatkacenko132@gmail.com>
2021-04-12 21:55:23 +0300

    das

--- js/common.js
+++ js/common.js
@@ -4,3 +4,12 @@ function f_acc() {
     $('.accordeon .acc-body').not($(this).next()).slideUp(200);
     $(this).next().slideToggle(200);
 }
+
+var btn = $('.hide_btn_wrap');
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 500) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
